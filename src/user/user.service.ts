5a54b3f115adccc80521f55b1edddc6889a83476
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-06 14:39:35 -0300

    Criando a aplicação do zero | NestJS com GraphQL #2

---
+++ src/user/user.service.ts
@@ -0,0 +1,57 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAllUser(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findUserById(id: string): Promise<User> {
+    const user = await this.userRepository.findOne(id);
+
+    if (!user) throw new NotFoundException('Usuário não encontrado');
+
+    return user;
+  }
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = await this.userRepository.create(data);
+
+    const userSaved = await this.userRepository.save(user);
+
+    if (!userSaved)
+      throw new InternalServerErrorException('Problema para criar um usuário.');
+
+    return userSaved;
+  }
+
+  async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+    const user = await this.findUserById(id);
+
+    await this.userRepository.update(user, { ...data });
+
+    return this.userRepository.create({ ...user, ...data });
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const user = await this.findUserById(id);
+
+    const deleted = await this.userRepository.delete(user);
+
+    return !deleted;
+  }
+}
